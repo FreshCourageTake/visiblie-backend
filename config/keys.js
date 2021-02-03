9a986cc033488085acae1a494822cb4bf91f8cf5
Andrew Garver <andrewsgarver@gmail.com>
2021-02-03 09:04:30 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-	mongoURI: 'mongodb+srv://andrewsgarver:8mUmhrNkp8NAHoBb@cluster0-mldkn.mongodb.net/test?retryWrites=true&w=majority',
+	mongoURI: '',
 	secretOrKey: 'secret'
 };
